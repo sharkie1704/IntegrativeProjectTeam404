25995bff46e8c6f3834875270b1c489f09ffe7c5
Hongyan Li <2254403@edu.vaniercollege.qc.ca>
2024-03-05 09:15:53 -0500

    Template

---
+++ LightMatrixAction/src/main/java/edu/vanier/template/MainApp.java
@@ -0,0 +1,16 @@
+package edu.vanier.template;
+
+ 
+/**
+ * This is a JavaFX project template to be used for creating GUI applications.
+ * JavaFX 20.0.2 is already linked to this project in the build.gradle file.
+ * @link: https://openjfx.io/javadoc/20/
+ * @see: Build Scripts/build.gradle
+ * @author Sleiman Rabah.
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello there!");
+    }
+}
