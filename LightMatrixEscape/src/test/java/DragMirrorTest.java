9dc59a6eddd41e6867ef763e0e43fae6ac6e9e7e
VStudent2000 <131478750+VStudent2000@users.noreply.github.com>
2024-04-16 06:12:25 -0400

    feature (mirror)
    The test for the drag feature of mirrors has been made. The object (mirrors) won,t be able to go beyond the pane where it resides and won't be able to be set on other objects

---
+++ LightMatrixEscape/src/test/java/DragMirrorTest.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ntela
+ */
+public class DragMirrorTest {
+//    
+//    ///// The Lower section is for moving a object \\\\\
+//        
+//    
+//        double originalXCoord = rect.getTranslateX();
+//        double originalYCoord = rect.getTranslateY();
+//        
+//        // - Takes coordinates of the object in the scene when pressed
+//        rect.setOnMousePressed(event->{
+//            xCoord = event.getSceneX()-rect.getTranslateX();
+//            yCoord = event.getSceneY()-rect.getTranslateY();
+//            
+//        });
+//        
+//        // - Changes coordinates of the object in the scene when dragged
+//        rect.setOnMouseDragged(event->{
+//            double moveX = event.getSceneX()- xCoord;
+//            rect.setTranslateX(moveX);
+//            
+//            if(rect.getBoundsInParent().getMinX()< 0){
+//                rect.setTranslateX(-(midPane.getLayoutBounds().getWidth())/2 + rect.getWidth()/2);
+//            }
+//            
+//            if(rect.getBoundsInParent().getMaxX()> midPane.getLayoutBounds().getWidth()){
+//                rect.setTranslateX((midPane.getLayoutBounds().getWidth())/2 - rect.getWidth()/2);
+//            }
+//                
+//            
+//            double moveY = event.getSceneY()- yCoord;
+//            rect.setTranslateY(moveY);
+//            
+//            if(rect.getBoundsInParent().getMinY()< 0){
+//                rect.setTranslateY(-(midPane.getLayoutBounds().getHeight())/2 + rect.getHeight()/2);
+//            }
+//            
+//            if(rect.getBoundsInParent().getMaxY()> midPane.getLayoutBounds().getHeight()){
+//                rect.setTranslateY((midPane.getLayoutBounds().getHeight())/2 - rect.getHeight()/2);
+//            }
+//            
+//        });
+//        
+//                
+//        // - Puts back object in original coordnates if in contact with another object
+//        rect.setOnMouseReleased(event->{
+//        if(rect.getBoundsInParent().intersects(c1.getBoundsInParent()) ||
+//                rect.getBoundsInParent().intersects(c2.getBoundsInParent())||
+//                rect.getBoundsInParent().intersects(c3.getBoundsInParent())||
+//                rect.getBoundsInParent().intersects(c4.getBoundsInParent())
+//                ){
+//                    rect.setTranslateX(originalXCoord);
+//                    rect.setTranslateY(originalYCoord);
+//                }
+//            
+//        });
+//        
+//    }
+}
